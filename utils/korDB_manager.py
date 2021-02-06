66b99320e0f2e1a8890485c3e63c9c02049254d5
coolseaweed <jeellato@gmail.com>
2021-02-07 04:27:57 +0900

    2021-02-07

--- utils/korDB_manager.py
+++ utils/korDB_manager.py
@@ -1,90 +1,110 @@
 import os
 import pandas as pd
+import requests
+import pymysql
 from pandas_datareader import data as pdr_data
 from bs4 import BeautifulSoup
 from datetime import datetime
-import requests
 
 
-
-KOREA_CODE_URL='https://dev-kind.krx.co.kr/corpgeneral/corpList.do?method=download&searchType=13'
+PWD='ehfvkfdl123@'
+DB_NAME='KOR_DB'
+KRX_URL='http://kind.krx.co.kr/corpgeneral/corpList.do?method=download&searchType=13'
 NAVER_URL_BASE='https://finance.naver.com/item/sise_day.nhn?code'#068270&page=1'
 USER_AGENT='Mozilla/5.0'
+
+
 
 class KoreaDB_manager():
 
 
     def __init__(self, vnet=False):
         """ constructor: MariaDB connection & code dictionary generation  """
-        self.stock_type = {
-            'kospi': 'stockMkt',
-            'kosdaq': 'kosdaqMkt'
-            } 
-        self.kospi_codes=None
-        self.kosdaq_codes=None
 
-        self.code_df=None
+        self.connection = pymysql.connect(host='localhost', port=3306, db=DB_NAME, 
+            user='root', passwd=PWD, autocommit=True)  
+
+        with self.connection.cursor() as cursor:
+            sql = """
+            CREATE TABLE IF NOT EXISTS company_info (
+                code VARCHAR(20),
+                company VARCHAR(40),
+                last_update DATE,
+                PRIMARY KEY (code))
+            """
+            cursor.execute(sql)
+            sql = """
+            CREATE TABLE IF NOT EXISTS daily_price (
+                code VARCHAR(20),
+                date DATE,
+                open BIGINT(20),
+                high BIGINT(20),
+                low BIGINT(20),
+                close BIGINT(20),
+                diff BIGINT(20),
+                volume BIGINT(20),
+                PRIMARY KEY (code, date))
+            """
+            cursor.execute(sql)
+
+        self.connection.commit()
+        self.code_dict = dict()
 
 
     def __del__(self):
         """ destructor: MariaDB disconnection   """
-
-        pass
-
+        self.connection.close() 
 
 
-    def addCodeList(self, code_path):
-        """ data url: https://dev-kind.krx.co.kr/main.do?method=loadInitPage&scrnmode=1 """
+    def getKRXcodes(self):
+        """ KRX로부터 상장기업 목록 파일을 읽어와서 데이터프레임으로 반환 """
+        url = KRX_URL
+        krx = pd.read_html(url, header=0)[0]
+        krx = krx[['종목코드', '회사명']]
+        krx = krx.rename(columns={'종목코드': 'code', '회사명': 'company'})
+        krx.code = krx.code.map('{:06d}'.format)
+        return krx
 
-        path = code_path
-        if not os.path.exists(path):
-            path = KOREA_CODE_URL
-        try:
-            df = pd.read_html(path)[0]
-            df = df.rename(columns={'회사명': 'name', '종목코드': 'code'})
-            df.code = df.code.map('{:06d}'.format)
-            self.code_df = df.sort_values(by='name',ignore_index=True)
+    def updateCompanyInfo(self):
+        """ 종목코드를 company_info 테이블에 업데이트 한 후 딕셔너리에 저장 """
 
-        except Exception as e: 
-            print('=================================================')
-            print(f'[ERROR] {__name__} : {e}') 
-            print(f'Cannot fetch to code data. Please check code path or url\npath:{code_path}\nurl:{KOREA_CODE_URL}')
-            print('=================================================')
+        sql = "SELECT * FROM company_info"
+        df = pd.read_sql(sql, self.connection)
+        for idx in range(len(df)):
+            self.code_dict[df['code'].values[idx]] = df['company'].values[idx]
+                    
+        with self.connection.cursor() as curs:
+            sql = "SELECT max(last_update) FROM company_info"
+            curs.execute(sql)
+            rs = curs.fetchone()
+            today = datetime.today().strftime('%Y-%m-%d')
+            if rs[0] == None or rs[0].strftime('%Y-%m-%d') < today:
+                krx = self.getKRXcodes()
+                for idx in range(len(krx)):
+                    code = krx.code.values[idx]
+                    company = krx.company.values[idx]                
+                    sql = f"REPLACE INTO company_info (code, company, last"\
+                        f"_update) VALUES ('{code}', '{company}', '{today}')"
+                    curs.execute(sql)
+                    self.code_dict[code] = company
+                    tmnow = datetime.now().strftime('%Y-%m-%d %H:%M')
+                    print(f"[{tmnow}] #{idx+1:04d} REPLACE INTO company_info "\
+                        f"VALUES ({code}, {company}, {today})")
+                self.connection.commit()
+                print('')              
 
+
+    def getCode(self, target_name):
+        for code, name in self.code_dict.items():
+            if name == target_name: 
+                return code
+            else:
+                "Can't find code! please check company name again"
+
+    def getName(self, code):
+        return self.code_dict[code]
 
         
-    def getCodeList(self):
-        return self.code_df
-
-    def getCode(self, name):
-        return self.code_df.query("name=='{}'".format(name))['code'].to_string(index=False).strip()
-
-    def getName(self, name):
-        return self.code_df.query("code=='{}'".format(name))['name'].to_string(index=False).strip()
-        
-
-    def getCodeList_beta(self, market):
-        
-        market_type = self.stock_type[market]
-        download_link = 'http://kind.krx.co.kr/corpgeneral/corpList.do'
-        download_link = download_link + '?method=download'
-        download_link = download_link + '&marketType=' + market_type
-        df = pd.read_html(download_link, header=0)[0]
-
-        if market == 'kospi':
-            df['종목코드'] = df['종목코드'].map('{:06d}.KS'.format)
-            df = df.rename(columns={'회사명': 'name', '종목코드': 'code'})
-            self.kospi_codes = df
-
-            return self.kospi_codes
-
-        elif market == 'kosdaq':
-            df['종목코드'] = df['종목코드'].map('{:06d}.KQ'.format)
-            df = df.rename(columns={'회사명': 'name', '종목코드': 'code'})
-            self.kosdaq_codes = df
-
-            return self.kospi_codes
-
 
 
     # --------------------------- Data Crawling --------------------------- #
@@ -148,8 +168,80 @@ class KoreaDB_manager():
         # print(code)
         #return data.get_data_yahoo(code, start, end)
 
-if __name__ == 'main':
+    def replaceIntoDB(self, df, num, code, company):
+        """네이버에서 읽어온 주식 시세를 DB에 REPLACE"""
+        with self.connection.cursor() as curs:
+            for r in df.itertuples():
+                sql = f"REPLACE INTO daily_price VALUES ('{code}', "\
+                    f"'{r.date}', {r.open}, {r.high}, {r.low}, {r.close}, "\
+                    f"{r.diff}, {r.volume})"
+                curs.execute(sql)
+            self.connection.commit()
+            print('[{}] #{:04d} {} ({}) : {} rows > REPLACE INTO daily_'\
+                'price [OK]'.format(datetime.now().strftime('%Y-%m-%d'\
+                ' %H:%M'), num+1, company, code, len(df)))
+
+    def updateDailyPrice(self, pages_to_fetch):
+        """KRX 상장법인의 주식 시세를 네이버로부터 읽어서 DB에 업데이트"""  
+        for idx, code in enumerate(self.code_dict):
+            df = self.getDataFromNaver(code, self.code_dict[code], pages_to_fetch)
+            if df is None:
+                continue
+            self.replaceIntoDB(df, idx, code, self.code_dict[code])   
+
+
+if __name__ == '__main__':
 
     data_loader = KoreaDB_manager()
-    data_loader.execute_daily()
+    data_loader.updateCompanyInfo()
+    data_loader.updateDailyPrice(1)
+
+    #data_loader.execute_daily()
+
+
+
+
+
+
+
+
+    # --------------------------- TRASH CAN --------------------------- #
+
+
+    # def addCodeList(self, code_path):
+    #     """ data url: https://dev-kind.krx.co.kr/main.do?method=loadInitPage&scrnmode=1 """
+
+    #     path = code_path
+    #     if not os.path.exists(path):
+    #         path = KOREA_CODE_URL
+    #     try:
+    #         df = pd.read_html(path)[0]
+    #         df = df.rename(columns={'회사명': 'name', '종목코드': 'code'})
+    #         df.code = df.code.map('{:06d}'.format)
+    #         self.code_df = df.sort_values(by='name',ignore_index=True)
+
+    #     except Exception as e: 
+    #         print('=================================================')
+    #         print(f'[ERROR] {__name__} : {e}') 
+    #         print(f'Cannot fetch to code data. Please check code path or url\npath:{code_path}\nurl:{KOREA_CODE_URL}')
+    #         print('=================================================')
+
+
+    # def addCodeList(self, code_path):
+    #     """ data url: https://dev-kind.krx.co.kr/main.do?method=loadInitPage&scrnmode=1 """
+
+    #     path = code_path
+    #     if not os.path.exists(path):
+    #         path = KOREA_CODE_URL
+    #     try:
+    #         df = pd.read_html(path)[0]
+    #         df = df.rename(columns={'회사명': 'name', '종목코드': 'code'})
+    #         df.code = df.code.map('{:06d}'.format)
+    #         self.code_df = df.sort_values(by='name',ignore_index=True)
+
+    #     except Exception as e: 
+    #         print('=================================================')
+    #         print(f'[ERROR] {__name__} : {e}') 
+    #         print(f'Cannot fetch to code data. Please check code path or url\npath:{code_path}\nurl:{KOREA_CODE_URL}')
+    #         print('=================================================')
 
