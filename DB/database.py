1061fa8676440cc2308b980abe8090c302d1d3b4
coolseaweed <jeellato@gmail.com>
2022-03-16 15:28:20 +0900

    dummy update for PR

--- DB/database.py
+++ DB/database.py
@@ -51,7 +51,7 @@ class DataBase:
                            ] = companyInfo['company'].values[idx]
 
     def getDailyPrice(self, code, start_date=None, end_date=None):
-        """KRX 종목의 일별 시세를 데이터프레임 형태로 반환
+        """ KRX 종목의 일별 시세를 데이터프레임 형태로 반환
             - code       : KRX 종목코드('005930') 또는 상장기업명('삼성전자')
             - start_date : 조회 시작일('2020-01-01'), 미입력 시 1년 전 오늘
             - end_date   : 조회 종료일('2020-12-31'), 미입력 시 오늘 날짜
